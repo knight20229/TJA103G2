10078b949ea0e9c2c43caffaeb82493f53d3ba1b
YU <mikagura0@gmail.com>
2026-01-13 12:59:23 +0800

    小debug

--- src/main/resources/static/js/frontprod/home.js
+++ src/main/resources/static/js/frontprod/home.js
@@ -191,30 +191,6 @@ $(document).ready(function() {
 	qtyInput.on('input', updateTotal);
 	updateTotal();
 
-	// --------------------------
-	// 搜尋商品
-	// --------------------------
-	const searchForm = $('#searchForm');
-	const searchInput = $('#searchInput');
 
-	searchForm.on('submit', function(e) {
-		const value = searchInput.val().trim();
-		if (value === "") {
-			e.preventDefault();
-			alert("請輸入商品名稱");
-		} else if (!/^[\u4e00-\u9fa5]+$/.test(value)) {
-			e.preventDefault();
-			alert("只可搜尋商品名稱（中文）");
-		}
-	});
-
-
-	// Enter 鍵觸發搜尋
-	searchInput.on('keypress', function(e) {
-		if (e.which === 13) {
-			e.preventDefault();
-			searchBtn.click();
-		}
-	});
 
 });
